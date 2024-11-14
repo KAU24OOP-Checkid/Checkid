d385a4e2c74ba0691f286ccee8fc0a6e534e87ce
Heeflower5001 <gmltmd3tp@naver.com>
2024-11-14 15:33:41 +0900

    4 week

--- app/src/main/java/com/example/checkid/model/UserRepository.kt
+++ app/src/main/java/com/example/checkid/model/UserRepository.kt
@@ -1,7 +1,20 @@
 package com.example.checkid.model
 
+import ChildUser
+import ParentUser
 import User
 
 object UserRepository {
     var users:MutableList<User> = mutableListOf()
+
+    init {
+        val a = ChildUser("1", "1")
+        val b = ParentUser("2", "2")
+
+        a.parent = b
+        b.child = a
+
+        users.add(a)
+        users.add(b)
+    }
 }
